f971f3744ae06b046a7505f357cba8d3dc85e4a2
Reltre <tglippert@gmail.com>
2015-03-24 13:49:28 -0700

    App can now render card images.

--- tl-blackjack-webapp/main.rb
+++ tl-blackjack-webapp/main.rb
@@ -1,30 +1,43 @@
 require 'rubygems'
 require 'sinatra'
+require 'pry'
+require_relative 'helpers.rb'
 
-# use Rack::Session::Cookie, :key => 'rack.session',
-#                            :path => '/',
-#                            :secret => 'BVL57Q' 
+use Rack::Session::Cookie, :key => 'rack.session',
+                           :path => '/',
+                           :secret => 'BVL57Q' 
 
-enable :sessions
 
 get '/' do
-  session[:money] = 500
-  redirect 'set-name'
-end
-
-get '/set-name' do
   erb :name_form
 end
 
-
 post '/bet' do
   session[:username] = params[:username]
-  erb :bet_form
-end 
-
-post '/game' do 
-
+  session[:money] = 500
+  redirect 'bet'  
 end
+
+get '/bet' do 
+
+  erb :bet_form
+end
+
+post '/game' do
+  session[:deck] = create_deck
+  deal_cards
+  #binding.pry
+  redirect'game'
+end
+
+get '/game' do
+  erb :game
+end
+
+not_found do
+  erb :no_such_page
+end
+
 
 # get '/nested-template' do 
 #   erb :'users/info'
