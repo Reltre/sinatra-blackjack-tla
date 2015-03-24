9a5f8873c578ce3aeb7e839ff1caa58669e24e2f
Reltre <tglippert@gmail.com>
2015-03-23 18:16:30 -0700

    Added initial page and betting page.

--- tl-blackjack-webapp/main.rb
+++ tl-blackjack-webapp/main.rb
@@ -1,8 +1,31 @@
 require 'rubygems'
 require 'sinatra'
 
-set :sessions, true
+# use Rack::Session::Cookie, :key => 'rack.session',
+#                            :path => '/',
+#                            :secret => 'BVL57Q' 
+
+enable :sessions
+
+get '/' do
+  session[:money] = 500
+  redirect 'set-name'
+end
+
+get '/set-name' do
+  erb :name_form
+end
 
 
+post '/bet' do
+  session[:username] = params[:username]
+  erb :bet_form
+end 
 
+post '/game' do 
 
+end
+
+# get '/nested-template' do 
+#   erb :'users/info'
+# end 
