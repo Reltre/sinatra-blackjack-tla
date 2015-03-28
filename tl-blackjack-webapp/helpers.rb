23ea645b05456b3bbcbc95c172bf73f8fdcde031
Reltre <tglippert@gmail.com>
2015-03-27 20:54:45 -0700

    Player and Dealer turns finished

--- tl-blackjack-webapp/helpers.rb
+++ tl-blackjack-webapp/helpers.rb
@@ -1,34 +1,5 @@
-FACE_VALUES = {
-  "Two"   => 2,
-  "Three" => 3,
-  "Four"  => 4,
-  "Five"  => 5,
-  "Six"   => 6,
-  "Seven" => 7,
-  "Eight" => 8,
-  "Nine"  => 9,
-  "Ten"   => 10,
-  "Jack"  => 10,
-  "Queen" => 10,
-  "King"  => 10,
-  "Ace"   => 11
-}
-
-SUITS = ["clubs", "spades", "hearts", "diamonds"]
-FACE_VALUES.freeze
-SUITS.freeze
 
 helpers do 
-  def create_deck
-    cards = []
-    SUITS.each do |suit| 
-      FACE_VALUES.values.each do |value|
-        cards << [suit,value]
-      end
-    end
-    cards.shuffle!
-  end
-
   def deal_cards
     session[:player_hand] = []
     session[:dealer_hand] = []
@@ -36,6 +7,10 @@ helpers do
     session[:dealer_hand] << session[:deck].pop
     session[:player_hand] << session[:deck].pop
     session[:dealer_hand] << session[:deck].pop
+  end
+
+  def calculate_total(cards)
+    55
   end
 
 end
--- tl-blackjack-webapp/main.rb
+++ tl-blackjack-webapp/main.rb
@@ -1,44 +1,144 @@
 require 'rubygems'
 require 'sinatra'
 require 'pry'
-require_relative 'helpers.rb'
+
 
 use Rack::Session::Cookie, :key => 'rack.session',
                            :path => '/',
                            :secret => 'BVL57Q' 
 
 
-get '/' do
-  erb :name_form
+helpers do 
+  def calculate_total(cards)
+    result = cards.inject(0) do |sum,card| 
+      if card[1] == 'ace'
+        sum + 11
+      else 
+        sum + (card[1].to_i == 0 ? 10 : card[1]) 
+      end
+    end
+
+    cards.select{ |card| card[1] == 'ace' }.count.times do 
+      break if result <= 21
+      result -= 10 
+    end
+    result
+  end
+
+  def reset
+    session[:player_hand] = []
+    session[:dealer_hand] = []
+  end
+
+  def card_image(card)
+    "<img src=/images/cards/#{card.join('_')}.jpg  
+    align=\'middle\' style=\'width:162px;height:235px\' hspace=\'5\'>"
+  end
 end
 
-post '/bet' do
-  session[:username] = params[:username]
+before do
+  @show_hit_or_stay = true
+end
+
+get '/' do
+  # if session[:player_name]
+  #   redirect 'game'
+  # else
+    session[:message] = nil
+    redirect 'new_player'
+  # end
+end
+
+get '/new_player' do
+  erb :new_player
+end
+
+post '/new_player' do
+  if params[:player_name].empty?
+    @error = "Name is required"
+    halt erb(:new_player)
+  end
+  session[:player_name] = params[:player_name]
   session[:money] = 500
-  redirect 'bet'  
+  redirect 'bet' 
 end
 
 get '/bet' do 
-
   erb :bet_form
 end
 
-post '/game' do
-  session[:deck] = create_deck
-  deal_cards
-  #binding.pry
+post '/bet' do
+  session[:bet] = params[:bet].to_i  
+  session[:money] -= session[:bet]
   redirect'game'
 end
+ #= session[:dealer_hand].size == 2 ? true : false
+
+# not_found do
+#   erb :no_such_page
+# end
 
 get '/game' do
+  values = [2,3,4,5,6,7,8,9,10,'jack','queen','king','ace']
+  suits = ["clubs", "spades", "hearts", "diamonds"]
+  session[:deck] = suits.product(values).shuffle!
+  session[:player_hand] = []
+  session[:dealer_hand] = []
+  session[:dealer_hand] << session[:deck].pop
+  session[:player_hand] << session[:deck].pop
+  session[:dealer_hand] << session[:deck].pop
+  session[:player_hand] << session[:deck].pop
+  @show_card_cover = true
   erb :game
 end
 
-not_found do
-  erb :no_such_page
+post '/game/player/hit' do
+  session[:player_hand] << session[:deck].pop   
+  if calculate_total(session[:player_hand]) > 21
+    @error = 
+    "Sorry,#{session[:player_name]}
+    busted with a total of 
+    #{calculate_total session[:player_hand]}, 
+    you have $#{session[:money]} left." 
+    @show_hit_or_stay = false
+  end
+  # redirect 'game'
+  erb :game
 end
 
+post '/game/player/stay' do
+  @show_card_cover = false
+  @dealer_turn = true if calculate_total(session[:dealer_hand]) < 17 
+  #redirect '/game/comparison' if calculate_total(session[:dealer_hand]) >=17
+  @show_hit_or_stay = false
+  @stay = "You have chosen to stay."
+  erb :game 
+end
 
-# get '/nested-template' do 
-#   erb :'users/info'
+post '/game/dealer-turn' do
+  session[:dealer_hand] << session[:deck].pop
+  dealer_total = calculate_total(session[:dealer_hand])
+  
+  @blackjack = true if dealer_total == 21 && session[:dealer_hand].size == 2
+  if dealer_total > 21
+    @success = "The Dealer has busted, #{session[:player_name]} wins!"
+    @dealer_turn = false
+    halt erb(:game)
+  elsif dealer_total >= 17 && dealer_total <= 21 || @blackjack
+    redirect '/game/comparison'
+  end
+  @show_card_cover = false
+  @show_hit_or_stay = false
+  
+  @dealer_turn = true  
+  erb :game
+end
+
+get '/game/comparison' do
+  
+end
+
+# get '/dealer_turn' do 
+#    erb :game
+   
 # end 
